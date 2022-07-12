5032022dce32379f1e1d2419bfcc352f5bdcde60
Tesla <bolarinwaahmed22@gmail.com>
2022-07-12 12:03:50 +0100

    Removed existing firebase and added a new | used env to hide sensitive details

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyBw0sXA9cqZGqLfR5HrEUzACYHety_XREY",
-  authDomain: "abatoms-chat-app.firebaseapp.com",
-  projectId: "abatoms-chat-app",
-  storageBucket: "abatoms-chat-app.appspot.com",
-  messagingSenderId: "700454679018",
-  appId: "1:700454679018:web:6d5726c176037f4cce4293",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-
-
+export const auth = getAuth(app);
